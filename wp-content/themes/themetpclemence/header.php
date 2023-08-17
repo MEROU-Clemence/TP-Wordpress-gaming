4ebd48dc123a9be5ffa6c00f30e1accb20b7292c
Clemence Merou <clemence.merou@gmail.com>
2023-08-17 15:31:07 +0200

    debut du theme perso

--- wp-content/themes/themetpclemence/header.php
+++ wp-content/themes/themetpclemence/header.php
@@ -4,9 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thème custom</title>
+    <title>Thème TP</title>
     <!-- import de bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.17.0/font/bootstrap-icons.css" rel="stylesheet">
     <!-- import de style.css -->
     <!-- <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>"> -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() . "/style.css"; ?>">
@@ -14,11 +15,25 @@
 
 <body>
     <!-- Partie réservée au header -->
-    <header class="bg-success text-light p-3">
+    <header class="header-general">
+        <div class="en-tete-line">
+            <div>
+                <span><i class="bi bi-geo-alt custom-icon">Trouver Magasin</i></span>
+                <span><i class="bi bi-envelope custom-icon">mon-compte@gmail.com</i></span>
+            </div>
+            <div class="en-tete-links">
+                <span>Promotions du moment</span>
+                <div class="vertical-bar"></div>
+                <span>Services customisation</span>
+                <div class="vertical-bar"></div>
+                <span>Cadeaux</span>
+            </div>
+        </div>
         <a href="<?php echo home_url(); ?>">
-            <img class="logogamingpalace" src="../../uploads/2023/08/Logo-et-nom-du-site.png" alt="Logo de mon site">
+            <img class="logo-gaming-palace" src="<?php echo get_template_directory_uri(); ?>/uploads/2023/08/Logo-de-mon-site.png" alt="Logo de mon site">
         </a>
-        <em class="blog-description"><?php echo get_bloginfo('description'); ?></em>
+
+        <em><?php echo get_bloginfo('description'); ?></em>
 
         <!-- on affiche le menu -->
         <?php
